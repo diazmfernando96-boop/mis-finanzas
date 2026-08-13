f1934e0c729e72ec1e95558047b338b012818350
diazmfernando96-boop <diazmfernando96@gmail.com>
2026-08-13 17:24:39 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("activate", (e) => clients.claim());
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(clients.claim());
+});
+
 self.addEventListener("fetch", (e) => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
